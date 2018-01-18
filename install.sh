98c1f033183fed1b3503e905f7eb228b9d6e5108
John Shaver <john@jshaver.net>
2018-01-18 14:52:23 -0800

    Removed extra boostrap. Started on header. Added Material.io icons

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ wget -c https://code.jquery.com/jquery-3.2.1.slim.min.js -O js/jquery-3.2.1.slim
 
 mkdir -p css
 wget -c https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/spacelab/bootstrap.min.css -O css/bootstrap-spacelab-3.3.7.min.css
-wget -c https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap-grid.min.css -O css/bootstrap-grid.min.css
-wget -c https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css -O css/bootstrap.min.css
+wget -c https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/MaterialIcons-Regular.woff -O ./css/MaterialIcons-Regular.woff
+wget -c https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/MaterialIcons-Regular.woff2 -O ./css/MaterialIcons-Regular.woff2
+wget -c https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/MaterialIcons-Regular.ttf -O ./css/MaterialIcons-Regular.ttf
+wget -c https://raw.githubusercontent.com/google/material-design-icons/master/iconfont/MaterialIcons-Regular.eot -O ./css/MaterialIcons-Regular.eot
 
 curl -fsSL bit.ly/node-installer | bash -s -- --no-dev-deps
 npm install -g uglify-js
